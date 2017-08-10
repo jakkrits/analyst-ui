e2a34f656e747621556066a27a520eafe828404c
Lou Huang <lou@louhuang.com>
2017-08-10 17:50:20 -0400

    Cache downloaded data tiles to save on network requests

--- src/app/data.js
+++ src/app/data.js
@@ -1,6 +1,10 @@
 import protobuf from 'protobufjs'
-import { filter } from 'lodash'
+import { filter, uniqWith, isEqual } from 'lodash'
 import speedTileDescriptor from '../proto/speedtile.proto.json'
+import { getTileUrlSuffix } from '../lib/tiles'
+
+const STATIC_DATA_TILE_PATH = 'https://s3.amazonaws.com/speed-extracts/2017/0/'
+const tileCache = {}
 
 /**
  * Uses `protobuf.js` module to parse and read a `SpeedTile` protocol buffer.
@@ -57,6 +61,17 @@ export function consolidateTiles (tiles) {
 }
 
 /**
+ * Stores tiles in module's `tileCache` variable.
+ *
+ * Returns the original input.
+ */
+function cacheTiles (tiles) {
+  Object.assign(tileCache, tiles)
+
+  return tiles
+}
+
+/**
  * Fetches all requested OpenTraffic data tiles and concatenates them into
  * a single object. If tiles are cached, retrieve those instead of performing
  * the actual network request.
@@ -65,13 +80,28 @@ export function consolidateTiles (tiles) {
  * in cache do not need to be re-fetched. We may need to set a dynamic
  * cache limit based on available memory, if that's something we can determine.
  *
- * @param {Array<String>} urls - a list of URLs to fetch.
+ * @param {Array<Object>} ids - a list of ids in the format { level, tile }
  * @return {Promise} - resolved with an object where tiles with level and
  *            tile index mapped to a nested key structure.
  */
-export function fetchDataTiles (urls) {
-  const promises = urls.map(url =>
-    window.fetch(url)
+export function fetchDataTiles (ids) {
+  // Obtain a list of ids of just level and tile, which allows us to easily
+  // filter out duplicates.
+  const simpleIds = ids.map(id => { return { level: id.level, tile: id.tile } })
+  const uniqueIds = uniqWith(simpleIds, isEqual)
+
+  const promises = uniqueIds.map(id => {
+    // If the id is already in tile cache, return it directly.
+    if (tileCache[id.level] && tileCache[id.level][id.tile]) {
+      return tileCache[id.level][id.tile]
+    }
+
+    // If the id is not in the cache, make a request for it.
+    const suffix = getTileUrlSuffix(id)
+    const subtile = 0
+    const url = `${STATIC_DATA_TILE_PATH}${suffix}.spd.${subtile}.gz`
+
+    return window.fetch(url)
       .then((response) => {
         // If a data tile fails to fetch, don't immediately reject; instead,
         // resolve with an error object. We'll deal with these later.
@@ -85,7 +115,7 @@ export function fetchDataTiles (urls) {
 
         return response.arrayBuffer()
       })
-  )
+  })
 
   return Promise.all(promises)
     // Reject from the responses all tiles that have errored out. Log the
@@ -111,4 +141,5 @@ export function fetchDataTiles (urls) {
     .then(array => array.sort((a, b) => a.startSegmentIndex - b.startSegmentIndex))
     // Consolidate all subtiles into a single object with lookup keys
     .then(consolidateTiles)
+    .then(cacheTiles)
 }
